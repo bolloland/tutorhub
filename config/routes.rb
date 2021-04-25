10854f450d00ed01edb3692177d3a7cf9de271df
Ryan Bollettino <ryan.bollettino@gmail.com>
2021-04-24 23:00:54 -0500

    created views:less, stud, tut

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,8 @@
 Rails.application.routes.draw do
-  get 'home/index'
   resources :lessons
   resources :students
   resources :tutors
 
-  
+  root "welcome#index"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
