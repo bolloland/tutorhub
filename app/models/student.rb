75bcd67508a63318d6a2e91bcaea674a3ae542d4
Ryan Bollettino <ryan.bollettino@gmail.com>
2021-04-29 17:00:11 -0500

    Student validations

--- app/models/student.rb
+++ app/models/student.rb
@@ -2,4 +2,7 @@ class Student < ApplicationRecord
   has_many :lessons
   has_many :tutors, through: :lessons
   has_secure_password
+
+  validates :first_name, :last_name, :email, :username, presence: true 
+  validates :email, :username, uniqueness: true 
 end
