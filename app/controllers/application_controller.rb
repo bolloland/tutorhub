2f60f5c668f30f1da5ae6685dfdea118e4b7a674
Ryan Bollettino <ryan.bollettino@gmail.com>
2021-04-29 16:57:54 -0500

    def current_user in AppCtrl

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,17 @@
 class ApplicationController < ActionController::Base
+
+    private
+    
+    def current_user  
+        if Tutor.find_by(id: session[:tutor_id])
+            @current_user ||= Tutor.find_by(id: session[:tutor_id])  
+        else
+            @current_user ||= Student.find_by(id: session[:student_id])  
+        end
+    end
+
+    def logout 
+        session.clear
+    end
+
 end
