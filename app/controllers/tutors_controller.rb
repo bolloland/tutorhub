8d358b261ec96ac5f788628916b6e79de4e4a79a
Ryan Bollettino <ryan.bollettino@gmail.com>
2021-04-29 16:58:54 -0500

    tutor controller actions

--- app/controllers/tutors_controller.rb
+++ app/controllers/tutors_controller.rb
@@ -1,5 +1,5 @@
 class TutorsController < ApplicationController
-before_action :get_tutor, only: [:show, :edit, :update, :delete]
+# before_action :get_tutor, only: [:show, :edit, :update, :delete]
 
     def index
         @tutors = Tutor.all
@@ -10,23 +10,43 @@ before_action :get_tutor, only: [:show, :edit, :update, :delete]
     end
 
     def show
+        @tutor = Tutor.find_by_id(params[:id])
     end
 
     def create 
+        @tutor = Tutor.new(tutor_params)
+            if @tutor.save
+                redirect_to tutor_path(@tutor)
+            else
+                render :new 
+        end
     end
 
     def edit 
+        @tutor = Tutor.find_by_id(params[:id])
     end
 
     def update 
+        @tutor = Tutor.find_by_id(params[:id])
+            if @tutor.update(tutor_params)
+                redirect_to tutor_path(@tutor)
+            else
+                render :edit
+        end
     end
 
     def delete 
+        @tutor = Tutor.find_by_id(params[:id])
         @tutor.destroy
         redirect_to tutors_path
     end
 
     private
+    
+    def current_user  
+        @current_user ||= session[:current_user_id] && 
+        Tutor.find_by(id: session[:current_user_id])  
+    end
 
     def get_tutor
         @tutor = Tutor.find_by_id(params[:id])
@@ -34,7 +54,7 @@ before_action :get_tutor, only: [:show, :edit, :update, :delete]
 
 
     def tutor_params
-        params.require(:tutor)
+        params.require(:tutor).permit(:id, :first_name, :last_name, :email, :username, :password, :years_exp, :subject_taught, :tutor_bio)
     end
     
 end
