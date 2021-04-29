af97c6878cf5ed54f9a4a413627faef027005e19
Ryan Bollettino <ryan.bollettino@gmail.com>
2021-04-29 17:00:28 -0500

    Tutor validations

--- app/models/tutor.rb
+++ app/models/tutor.rb
@@ -3,6 +3,6 @@ class Tutor < ApplicationRecord
   has_many :students, through: :lessons
   has_secure_password
 
-  validates :first_name, :last_name, :email, :username, presence: :true 
-  validates :email, :username, uniqueness: :true 
+  validates :first_name, :last_name, :email, :username, presence: true 
+  validates :email, :username, uniqueness: true 
 end
