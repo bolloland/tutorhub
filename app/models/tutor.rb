98001418ade920f4c664844084bae25bb6a47238
Ryan Bollettino <ryan.bollettino@gmail.com>
2021-05-04 23:47:28 -0500

    def omniauth Tutor#class

--- app/models/tutor.rb
+++ app/models/tutor.rb
@@ -5,4 +5,17 @@ class Tutor < ApplicationRecord
 
   validates :first_name, :last_name, :email, :username, presence: true 
   validates :email, :username, uniqueness: true 
+
+  scope :alpha_by_subject, -> { student.order(subject_help: :asc) }
+
+  def self.from_omniauth(response)
+    
+    Tutor.find_or_create_by(uid: response[:uid], provider: response[:provider]) do |x|
+        x.username = response[:info][:first_name]
+        x.first_name = response[:info][:first_name]
+        x.last_name = response[:info][:last_name]
+        x.email = response[:info][:email]
+        x.password = SecureRandom.hex(15)
+    end
+  end 
 end
