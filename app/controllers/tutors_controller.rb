1ded03bbc01126634aa5a96d5e127c0e4232eae6
Ryan Bollettino <ryan.bollettino@gmail.com>
2021-05-09 02:51:37 -0500

    added before_actions on Tutors#controller

--- app/controllers/tutors_controller.rb
+++ app/controllers/tutors_controller.rb
@@ -1,10 +1,11 @@
 class TutorsController < ApplicationController
 # before_action :get_tutor, only: [:show, :edit, :update, :delete]
 before_action :current_tutor, except: [:new, :create]
+before_action :get_tutor, except: [:index, :new, :create]
+before_action :get_student, only: [:index, :show]
 
     def index
-        @student = Student.find_by_id(params[:student_id])
-        @tutors = Tutor.all
+        @tutors = Tutor.all.alpha_by_subject_taught.order_by_years_exp
 
     end
    
@@ -13,8 +14,6 @@ before_action :current_tutor, except: [:new, :create]
     end
 
     def show
-        @tutor = Tutor.find_by_id(params[:id])
-        @student = Student.find_by_id(params[:student_id])
 
     end
 
@@ -30,11 +29,9 @@ before_action :current_tutor, except: [:new, :create]
     end
 
     def edit 
-        @tutor = Tutor.find_by_id(params[:id])
     end
 
     def update 
-        @tutor = Tutor.find_by_id(params[:id])
             if @tutor.update(tutor_params)
                 redirect_to tutor_path(@tutor)
             else
@@ -43,24 +40,27 @@ before_action :current_tutor, except: [:new, :create]
     end
 
     def delete 
-        @tutor = Tutor.find_by_id(params[:id])
         @tutor.destroy
         redirect_to tutors_path
     end
 
     private
     
-    # def current_user  
-        # @current_user ||= session[:current_user_id] && 
-        # Tutor.find_by(id: session[:current_user_id])  
-    # end
-
     def get_tutor
         @tutor = Tutor.find_by_id(params[:id])
     end
-
+    
+    def get_student
+        @student = Student.find_by_id(params[:student_id])
+    end
+    
     def tutor_params
         params.require(:tutor).permit(:id, :first_name, :last_name, :email, :username, :password, :years_exp, :subject_taught, :tutor_bio)
     end
     
 end
+
+# def current_user  
+    # @current_user ||= session[:current_user_id] && 
+    # Tutor.find_by(id: session[:current_user_id])  
+# end
