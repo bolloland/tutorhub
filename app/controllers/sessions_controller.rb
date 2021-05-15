3052589595a98ea7d7498a3065a5eb50ce4f108f
Ryan Bollettino <ryan.bollettino@gmail.com>
2021-05-15 08:14:35 -0500

    clean-up

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,9 +4,10 @@ class SessionsController < ApplicationController
   def tutor_new #tutor-Login form rendered
     @tutor = Tutor.new
   end
-
+  
   def student_new
     @student = Student.new
+     #attach the sessions to the user
   end
 
   def tutor_omniauth
@@ -21,50 +22,39 @@ class SessionsController < ApplicationController
       end
   end  
     
- 
-  # def student_omniauth
-  #   @student = Student.from_omniauth(request.env['omniauth.auth'])
-  #   if @student.valid?
-  #     session[:student_id] = student.id
-  #     current_student
-  #     redirect_to student_path(@student)
-  #   else
-  #     render "student_new"
-  #   end
-  # end
-
-  def tutor_create #attach the sessions to the user
+  
+  def tutor_create
     @tutor = Tutor.find_by(username: params[:session][:username])
-      if @tutor && @tutor.authenticate(params[:session][:password])
-        session[:tutor_id] = @tutor.id 
-        current_tutor
-        redirect_to tutor_path(@tutor)
-      else
-        redirect_to new_tutor_path      
-      end
+    if @tutor && @tutor.authenticate(params[:session][:password])
+      session[:tutor_id] = @tutor.id 
+      current_tutor
+      redirect_to tutor_path(@tutor)
+    else
+      redirect_to new_tutor_path      
+    end
   end
-
+  
   def student_create #attach the sessions to the user
     @student = Student.find_by(username: params[:session][:username])
-      if @student && @student.authenticate(params[:session][:password])
-        session[:student_id] = @student.id 
-        current_student
-        redirect_to student_path(@student)
-      else
-        render "student_new"
-      end
+    if @student && @student.authenticate(params[:session][:password])
+      session[:student_id] = @student.id 
+      current_student
+      redirect_to student_path(@student)
+    else
+      render "student_new"
+    end
   end
-
+  
   def tutor_destroy
     session.clear
     redirect_to root_path
   end
-
+  
   def student_destroy
     session.clear
     redirect_to root_path
   end
-
+  
   def tutor_params
     params.require(:tutor).permit(:id, :username, :password)
   end 
@@ -73,3 +63,15 @@ class SessionsController < ApplicationController
     params.require(:student).permit(:id, :username, :password)
   end 
 end
+
+
+ # def student_omniauth
+ #   @student = Student.from_omniauth(request.env['omniauth.auth'])
+ #   if @student.valid?
+ #     session[:student_id] = student.id
+ #     current_student
+ #     redirect_to student_path(@student)
+ #   else
+ #     render "student_new"
+ #   end
+ # end
--- config/routes.rb
+++ config/routes.rb
@@ -1,23 +1,21 @@
 Rails.application.routes.draw do
-  devise_for :users
   resources :students do
     resources :tutors do
       resources :lessons
-      
     end
   end
-    
+  
   resources :tutors do
     resources :students do
       resources :lessons 
-      
     end
   end
-      
+  
   resources :students
   resources :tutors
   resources :lessons
-
+  
+  devise_for :users
 
   get 'auth/:provider/callback' => 'sessions#tutor_omniauth'
   # get 'auth/:provider/callback' => 'sessions#student_omniauth'
